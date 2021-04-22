12b39007a21efd6740071e4e85e02882d86f8324
James Liu <jamesliu96@users.noreply.github.com>
2021-04-22 16:11:02 +0800

    build

---
+++ build/chime.js
@@ -0,0 +1,214 @@
+const b2t = (beats, tempo) => 60 / (beats * tempo);
+const noteFrequency = {
+    C0: 16.35,
+    D0: 18.35,
+    E0: 20.6,
+    F0: 21.83,
+    G0: 24.5,
+    A0: 27.5,
+    B0: 30.87,
+    C1: 32.7,
+    D1: 36.71,
+    E1: 41.2,
+    F1: 43.65,
+    G1: 49,
+    A1: 55,
+    B1: 61.74,
+    C2: 65.41,
+    D2: 73.42,
+    E2: 82.41,
+    F2: 87.31,
+    G2: 98,
+    A2: 110,
+    B2: 123.47,
+    C3: 130.81,
+    D3: 146.83,
+    E3: 164.81,
+    F3: 174.61,
+    G3: 196,
+    A3: 220,
+    B3: 246.94,
+    C4: 261.63,
+    D4: 293.66,
+    E4: 329.63,
+    F4: 349.23,
+    G4: 392,
+    A4: 440,
+    B4: 493.88,
+    C5: 523.25,
+    D5: 587.33,
+    E5: 659.25,
+    F5: 698.46,
+    G5: 783.99,
+    A5: 880,
+    B5: 987.77,
+    C6: 1046.5,
+    D6: 1174.66,
+    E6: 1318.51,
+    F6: 1396.91,
+    G6: 1567.98,
+    A6: 1760,
+    B6: 1975.53,
+    C7: 2093,
+    D7: 2349.32,
+    E7: 2637.02,
+    F7: 2793.83,
+    G7: 3135.96,
+    A7: 3520,
+    B7: 3951.07,
+    C8: 4186.01,
+    D8: 4698.63,
+    E8: 5274.04,
+    F8: 5587.65,
+    G8: 6271.93,
+    A8: 7040,
+    B8: 7902.13,
+    Db0: 17.32,
+    'C#0': 17.32,
+    Eb0: 19.45,
+    'D#0': 19.45,
+    Gb0: 23.12,
+    'F#0': 23.12,
+    Ab0: 25.96,
+    'G#0': 25.96,
+    Bb0: 29.14,
+    'A#0': 29.14,
+    Db1: 34.65,
+    'C#1': 34.65,
+    Eb1: 38.89,
+    'D#1': 38.89,
+    Gb1: 46.25,
+    'F#1': 46.25,
+    Ab1: 51.91,
+    'G#1': 51.91,
+    Bb1: 58.27,
+    'A#1': 58.27,
+    Db2: 69.3,
+    'C#2': 69.3,
+    Eb2: 77.78,
+    'D#2': 77.78,
+    Gb2: 92.5,
+    'F#2': 92.5,
+    Ab2: 103.83,
+    'G#2': 103.83,
+    Bb2: 116.54,
+    'A#2': 116.54,
+    Db3: 138.59,
+    'C#3': 138.59,
+    Eb3: 155.56,
+    'D#3': 155.56,
+    Gb3: 185,
+    'F#3': 185,
+    Ab3: 207.65,
+    'G#3': 207.65,
+    Bb3: 233.08,
+    'A#3': 233.08,
+    Db4: 277.18,
+    'C#4': 277.18,
+    Eb4: 311.13,
+    'D#4': 311.13,
+    Gb4: 369.99,
+    'F#4': 369.99,
+    Ab4: 415.3,
+    'G#4': 415.3,
+    Bb4: 466.16,
+    'A#4': 466.16,
+    Db5: 554.37,
+    'C#5': 554.37,
+    Eb5: 622.25,
+    'D#5': 622.25,
+    Gb5: 739.99,
+    'F#5': 739.99,
+    Ab5: 830.61,
+    'G#5': 830.61,
+    Bb5: 932.33,
+    'A#5': 932.33,
+    Db6: 1108.73,
+    'C#6': 1108.73,
+    Eb6: 1244.51,
+    'D#6': 1244.51,
+    Gb6: 1479.98,
+    'F#6': 1479.98,
+    Ab6: 1661.22,
+    'G#6': 1661.22,
+    Bb6: 1864.66,
+    'A#6': 1864.66,
+    Db7: 2217.46,
+    'C#7': 2217.46,
+    Eb7: 2489.02,
+    'D#7': 2489.02,
+    Gb7: 2959.96,
+    'F#7': 2959.96,
+    Ab7: 3322.44,
+    'G#7': 3322.44,
+    Bb7: 3729.31,
+    'A#7': 3729.31,
+    Db8: 4434.92,
+    'C#8': 4434.92,
+    Eb8: 4978.03,
+    'D#8': 4978.03,
+    Gb8: 5919.91,
+    'F#8': 5919.91,
+    Ab8: 6644.88,
+    'G#8': 6644.88,
+    Bb8: 7458.62,
+    'A#8': 7458.62,
+};
+const toFreq = (key = 0) => typeof key === 'number'
+    ? key
+    : noteFrequency[`${key.slice(0, 1).toUpperCase()}${key.slice(1)}`] || 0;
+export var Chime;
+(function (Chime) {
+    function playNotes(notes, tempo, type, outputNode) {
+        return new Promise((resolve) => {
+            if (!notes.length)
+                return;
+            const { oscillator, gain } = initNodes(type, outputNode, notes.reduce((acc, [_, beats]) => acc + b2t(beats, tempo), 0), resolve);
+            notes.reduce((from, [frequency, beats, volume, detune]) => {
+                const to = from + b2t(beats, tempo);
+                oscillate(oscillator, gain, toFreq(frequency), from, volume, detune);
+                oscillate(oscillator, gain, 0, to, volume, detune);
+                return to;
+            }, 0);
+        });
+    }
+    Chime.playNotes = playNotes;
+    function playData(data, type, outputNode, absolute = false) {
+        return new Promise((resolve) => {
+            if (!data.length)
+                return;
+            const { oscillator, gain } = initNodes(type, outputNode, Math.max(...data.map(([_, time]) => time)), resolve);
+            data.forEach(([frequency, startTime, durationOrEndTime, volume, detune]) => {
+                oscillate(oscillator, gain, toFreq(frequency), startTime, volume, detune);
+                oscillate(oscillator, gain, 0, absolute ? durationOrEndTime : startTime + durationOrEndTime, volume, detune);
+            });
+        });
+    }
+    Chime.playData = playData;
+    function initNodes(type, outputNode, stopTime, resolve) {
+        const audioContext = outputNode.context;
+        const oscillator = audioContext.createOscillator();
+        if (type instanceof PeriodicWave) {
+            oscillator.setPeriodicWave(type);
+        }
+        else {
+            oscillator.type = type;
+        }
+        oscillator.start();
+        oscillator.stop(stopTime);
+        const gain = audioContext.createGain();
+        oscillator.connect(gain);
+        gain.connect(outputNode);
+        oscillator.onended = () => {
+            gain.disconnect();
+            oscillator.disconnect();
+            resolve();
+        };
+        return { oscillator, gain };
+    }
+    function oscillate(oscillator, gain, frequency, from, volume = 1, detune = 0) {
+        oscillator.frequency.setValueAtTime(frequency, from);
+        oscillator.detune.setValueAtTime(detune, from);
+        gain.gain.setValueAtTime(volume, from);
+    }
+})(Chime || (Chime = {}));
---
+++ build/main.js
@@ -0,0 +1,212 @@
+import { Chime } from './chime.js';
+import { KirbyBass1, KirbyBass2, KirbySynth1, KirbySynth2, } from './songs/kirby.js';
+import { MarioBass1, MarioBass2, MarioSynth1, MarioSynth2, } from './songs/mario.js';
+import { MetroidSynth1, MetroidSynth2 } from './songs/metroid.js';
+import { ZeldaBass1, ZeldaBass2, ZeldaSynth1, ZeldaSynth2, } from './songs/zelda.js';
+const [width, height] = [600, 300];
+let currentContext = null;
+let currentAnaysers = [];
+const mapper = ({ name, time, duration, velocity, }) => [
+    name,
+    time,
+    duration,
+    velocity,
+    0,
+];
+function playNotesSong(context) {
+    const analyser = context.createAnalyser();
+    Chime.playNotes([
+        ['c4', 4],
+        ['d4', 4],
+        ['e4', 4],
+        ['c4', 4],
+        ['c4', 4],
+        ['d4', 4],
+        ['e4', 4],
+        ['c4', 4],
+        ['e4', 4, 0.5],
+        ['f4', 4, 0.5],
+        ['g4', 4, 0.5],
+        [0, 4],
+        ['e4', 4],
+        ['f4', 4],
+        ['g4', 4],
+    ], 60, 'sine', analyser);
+    return [analyser];
+}
+const randgen = () => Math.random() * 2 - 1;
+function playDataKirby(context) {
+    const analyser = context.createAnalyser();
+    const analyser1 = context.createAnalyser();
+    const analyser2 = context.createAnalyser();
+    const analyser3 = context.createAnalyser();
+    const analyser4 = context.createAnalyser();
+    analyser1.connect(analyser);
+    analyser2.connect(analyser);
+    analyser3.connect(analyser);
+    analyser4.connect(analyser);
+    Chime.playData(KirbyBass1.map(mapper), 'triangle', analyser1);
+    const a = [...Array(32768)].map(randgen);
+    const b = a.slice();
+    b.unshift(b.pop());
+    Chime.playData(KirbyBass2.map(mapper), context.createPeriodicWave(a, b), analyser2);
+    Chime.playData(KirbySynth1.map(mapper), 'square', analyser3);
+    Chime.playData(KirbySynth2.map(mapper), 'square', analyser4);
+    return [analyser, analyser1, analyser2, analyser3, analyser4];
+}
+function playDataMario(context) {
+    const analyser = context.createAnalyser();
+    const analyser1 = context.createAnalyser();
+    const analyser2 = context.createAnalyser();
+    const analyser3 = context.createAnalyser();
+    const analyser4 = context.createAnalyser();
+    analyser1.connect(analyser);
+    analyser2.connect(analyser);
+    analyser3.connect(analyser);
+    analyser4.connect(analyser);
+    Chime.playData(MarioBass1.map(mapper), 'triangle', analyser1);
+    const a = [...Array(32768)].map(randgen);
+    const b = a.slice();
+    b.unshift(b.pop());
+    Chime.playData(MarioBass2.map(mapper), context.createPeriodicWave(a, b), analyser2);
+    Chime.playData(MarioSynth1.map(mapper), 'square', analyser3);
+    Chime.playData(MarioSynth2.map(mapper), 'square', analyser4);
+    return [analyser, analyser1, analyser2, analyser3, analyser4];
+}
+function playDataZelda(context) {
+    const analyser = context.createAnalyser();
+    const analyser1 = context.createAnalyser();
+    const analyser2 = context.createAnalyser();
+    const analyser3 = context.createAnalyser();
+    const analyser4 = context.createAnalyser();
+    analyser1.connect(analyser);
+    analyser2.connect(analyser);
+    analyser3.connect(analyser);
+    analyser4.connect(analyser);
+    Chime.playData(ZeldaBass1.map(mapper), 'triangle', analyser1);
+    const a = [...Array(32768)].map(randgen);
+    const b = a.slice();
+    b.unshift(b.pop());
+    Chime.playData(ZeldaBass2.map((o) => ({ ...o, name: 'C4' })).map(mapper), context.createPeriodicWave(a, b), analyser2);
+    Chime.playData(ZeldaSynth1.map(mapper), 'square', analyser3);
+    Chime.playData(ZeldaSynth2.map(mapper), 'square', analyser4);
+    return [analyser, analyser1, analyser2, analyser3, analyser4];
+}
+function playDataMetroid(context) {
+    const analyser = context.createAnalyser();
+    const analyser1 = context.createAnalyser();
+    const analyser2 = context.createAnalyser();
+    analyser1.connect(analyser);
+    analyser2.connect(analyser);
+    Chime.playData(MetroidSynth1.map(mapper), 'square', analyser1);
+    Chime.playData(MetroidSynth2.map(mapper), 'square', analyser2);
+    return [analyser, analyser1, analyser2];
+}
+const controls = document.getElementById('controls');
+const btnStop = document.getElementById('btn-stop');
+[
+    playNotesSong,
+    playDataKirby,
+    playDataMario,
+    playDataZelda,
+    playDataMetroid,
+].forEach((fn) => {
+    const btn = document.createElement('button');
+    btn.id = `btn-${fn.name}`;
+    btn.innerText = fn.name;
+    btn.addEventListener('click', () => {
+        if (currentContext && currentContext.state !== 'closed')
+            currentContext.close();
+        currentContext = new AudioContext();
+        currentAnaysers = fn(currentContext);
+        currentAnaysers.forEach((analyser, idx) => {
+            if (!idx)
+                analyser.connect(currentContext.destination);
+            analyser.fftSize = 2 ** parseInt(slider.value);
+        });
+    });
+    controls.insertBefore(btn, btnStop);
+});
+btnStop.addEventListener('click', () => {
+    if (currentContext && currentContext.state !== 'closed')
+        currentContext.close();
+});
+const slider = document.getElementById('slider');
+const sliderV = document.getElementById('sliderv');
+slider.addEventListener('input', function () {
+    const v = 2 ** parseInt(this.value);
+    currentAnaysers.forEach((analyser) => {
+        analyser.fftSize = v;
+    });
+    sliderV.innerText = `${v}`;
+});
+const canvasWidth = width * devicePixelRatio;
+const canvasHeight = height * devicePixelRatio;
+const canvas = document.getElementById('osc');
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+canvas.style.width = `${width}px`;
+canvas.style.height = `${height}px`;
+canvas.style.backgroundColor = 'green';
+const canvases = document.getElementById('oscs');
+canvases.style.width = `${width}px`;
+canvases.style.height = `${height}px`;
+const ctx2dGroup = [canvas.getContext('2d')];
+['green', 'purple', 'purple', 'green'].forEach((color, idx) => {
+    const c = document.getElementById(`osc${idx + 1}`);
+    c.width = canvasWidth / 2;
+    c.height = canvasHeight / 2;
+    c.style.width = `${width / 2}px`;
+    c.style.height = `${height / 2}px`;
+    c.style.backgroundColor = color;
+    ctx2dGroup.push(c.getContext('2d'));
+});
+function draw() {
+    ctx2dGroup.forEach((ctx2d, idx) => {
+        const [w, h] = idx
+            ? [canvasWidth / 2, canvasHeight / 2]
+            : [canvasWidth, canvasHeight];
+        ctx2d.clearRect(0, 0, w, h);
+    });
+    if (!currentContext)
+        return;
+    currentAnaysers.forEach((analyser, idx) => {
+        const ctx2d = ctx2dGroup[idx];
+        if (ctx2d) {
+            const [w, h] = idx
+                ? [canvasWidth / 2, canvasHeight / 2]
+                : [canvasWidth, canvasHeight];
+            const data = new Uint8Array(analyser.fftSize);
+            analyser.getByteTimeDomainData(data);
+            const rects = [];
+            for (let i = 0; i < w; i++) {
+                const d = data[~~((i / w) * data.length)];
+                const dRate = d / 255;
+                const [x, y] = [i, dRate * h];
+                rects.push({ x, y });
+            }
+            ctx2d.strokeStyle = 'white';
+            ctx2d.lineJoin = 'round';
+            ctx2d.lineWidth = 1 * devicePixelRatio;
+            ctx2d.beginPath();
+            rects.forEach(({ x, y }, i) => {
+                if (i)
+                    ctx2d.lineTo(x, y);
+                else
+                    ctx2d.moveTo(x, y);
+            });
+            ctx2d.stroke();
+            if (!idx) {
+                ctx2d.font = `${16 * devicePixelRatio}px monospace`;
+                ctx2d.fillStyle = 'white';
+                ctx2d.textBaseline = 'hanging';
+                ctx2d.fillText(`${currentContext.currentTime.toFixed(3)}s`, 0, 0);
+            }
+        }
+    });
+}
+function loop() {
+    draw();
+    requestAnimationFrame(loop);
+}
+loop();
